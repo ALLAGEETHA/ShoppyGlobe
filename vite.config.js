d7f04d44aec6227aa2a43b2dcb72f19047ebf837
ALLAGEETHA <166376787+ALLAGEETHA@users.noreply.github.com>
2025-11-21 12:31:39 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-// vite.config.js
+// Vite configuration with React SWC plugin for fast builds
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()]
   base: '/SHOPPYGLOBE/',   // 👈 match the URL you see in GitHub Pages
 });
