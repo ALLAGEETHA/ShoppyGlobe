e3e582b7c4f7d3edd0a02f8519469d3c91eaf5b1
ALLAGEETHA <allageetha12@gmail.com>
2025-11-21 10:52:31 +0530

    docs: add comment explaining ProductList component functionality

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,3 +1,4 @@
+// ProductList component: displays products with search, category filters, and promotions
 import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import {
